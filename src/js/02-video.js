336752aed5a026e51b12c2528b424921182fe895
Zamder13 <Zamder@ukr.net>
2021-10-31 17:55:35 +0200

    Сделал дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle  from "lodash.throttle";
 
+
+const TIME = 'videoplayer-current-time'
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle((evt) => {
+    
+    localStorage.setItem(TIME, `${evt.seconds}`);
+    console.log(localStorage[TIME])
+  }, 1000))
+
+player.setCurrentTime(localStorage[TIME]);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,73 @@
+import throttle  from "lodash.throttle";
+const STORAGE_KEY = "feedback-form-state"
+const formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+
+}
+populateTextarea()
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', throttle(onInputHandle, 500));
+
+
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+
+    // console.log("write form");
+
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+ };
+
+
+
+
+
+function onInputHandle(evt) {
+    const message = evt.target.value;
+    formData[evt.target.name] = message;
+
+    // localStorage.setItem(STORAGE_KEY, message);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+
+
+
+    // console.log(message);
+    // console.log(formData);
+
+};
+
+
+function populateTextarea() {
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
+
+
+    if (savedMessage) {
+        savedMessage = JSON.parse(savedMessage)
+        
+        console.log(savedMessage);
+        Object.entries(savedMessage).forEach(([name, value]) => {
+            formData[name] = value;
+            refs.form.elements[name].value = value;
+            // console.log(value);
+        })
+     }
+
+    
+    
+   
+};
+ 
+
+
+
 
