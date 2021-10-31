0a5880b514ac2f71a094cb495c525a672f71ced9
Zamder13 <Zamder@ukr.net>
2021-10-31 12:39:28 +0200

    Исправил первую домашку

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,66 +1,39 @@
-// Add imports above this line
+
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+ 
 
 const galleryContainer = document.querySelector('.gallery');
 const itemMurkup = createImageElement(galleryItems);
 
+
+
 galleryContainer.insertAdjacentHTML('beforeend', itemMurkup)
 
-galleryContainer.addEventListener('click', onCLickImage)
 
-const instance = basicLightbox.create(`
-    <img src="" />
-`, {
-  onShow: () => {
-    window.addEventListener('keydown', keydownEscape);
-  },
-  onClose: () => {
-    window.removeEventListener('keydown', keydownEscape);
-  },
-});
-
-
-  function createImageElement(array) {
+ function createImageElement(array) {
         return array.map(({ preview, original, description }) => {
             return `
-    <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+    <div>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
 </div>
     `
         }).join('');
     }
 
-function keydownEscape(event) {
-    console.log(event);
-    if (event.key === 'Escape') {
-        instance.close();
-        return;
-    }
-}
 
 
-function onCLickImage(event) {
-    
-    event.preventDefault()
-    const bigImage = event.target.dataset.source
-    
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    }
- instance.element().querySelector('img').src = bigImage;
-  instance.show();
-
-}
+new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt",
+        captionPosition: 'bottom',
+        captionDelay: '250',
+        focus: false,
+       
+    });
